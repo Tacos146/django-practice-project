9e3daf36cccdffcb4b4dd40a7f6f4d0f5449c5ec
Tacos146 <takkiy2@gmail.com>
2025-01-09 21:55:54 -0500

    third

--- customer360/views.py
+++ customer360/views.py
@@ -14,7 +14,8 @@ def create_customer(request):
         email = request.POST["email"]
         phone = request.POST["phone"]
         address = request.POST["address"]
-        customer = Customer.objects.create(name=name,email=email,phone=phone,address=address)
+        social_media = request.POST["social_media"]
+        customer = Customer.objects.create(name=name,email=email,phone=phone,address=address,social_media=social_media)
         customer.save()
         msg = "Successfully Saved a Customer"
         return render(request,"add.html",context={"msg":msg})
@@ -57,6 +58,6 @@ def interact(request,cid):
     return render(request,"interact.html",context=context)
 
 
-    
+
 
 
